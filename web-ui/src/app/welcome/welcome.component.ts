cafffc4a6a9d9e5ee036269b1d2586a7e4977191
Sergey Kovalev <dipdeepcode@mail.ru>
2026-08-11 22:03:38 +0300

    feat(web-ui): очищена главная страница, добавлено кэширование в cicd

--- web-ui/src/app/welcome/welcome.component.ts
+++ web-ui/src/app/welcome/welcome.component.ts
@@ -1,26 +1,10 @@
-import { Component, DestroyRef, inject, signal } from '@angular/core';
-import { HalService } from '../core/services/hal.service';
-import { Workspace, UnpackedResourceCollection } from '../core/declarations';
-import { JsonPipe } from '@angular/common';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
-  imports: [JsonPipe],
+  imports: [],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css',
 })
 export class WelcomeComponent {
-  private readonly destroyRef = inject(DestroyRef);
-  private readonly halService = inject(HalService);
-  protected response = signal<UnpackedResourceCollection<Workspace> | null>(null);
-
-  protected getRootResourceCollection(): void {
-    this.halService
-      .getResourceCollection<Workspace>('workspaces')
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe({
-        next: (resp) => this.response.set(resp),
-      });
-  }
 }
