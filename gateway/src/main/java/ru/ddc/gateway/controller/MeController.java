19a626222d0682971178c7a9718238c79c892987
Sergey Kovalev <dipdeepcode@mail.ru>
2026-08-19 22:20:24 +0300

    feat(gateway): /login-options перенесено на /bff/login-options

--- gateway/src/main/java/ru/ddc/gateway/controller/MeController.java
+++ gateway/src/main/java/ru/ddc/gateway/controller/MeController.java
@@ -3,15 +3,17 @@ package ru.ddc.gateway.controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 import java.util.Objects;
 
 @RestController
+@RequestMapping("/bff")
 public class MeController {
 
-    @GetMapping("/bff/me")
+    @GetMapping("/me")
     public Map<String, Object> getUserInfo(@AuthenticationPrincipal OidcUser oidcUser) {
         if (oidcUser == null) {
             return Map.of("error", "Unauthorized");
