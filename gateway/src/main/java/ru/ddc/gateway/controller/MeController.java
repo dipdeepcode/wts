74ded2fa8aecaaf33eab1f9dfd25d3b62f5faa5a
Sergey Kovalev <dipdeepcode@mail.ru>
2026-08-23 15:31:49 +0300

    Reapply "feat(gateway): доработан MeController"
    This reverts commit 2e42f1cac5b649018d0de37a3980a4a588c3c9bf.

--- gateway/src/main/java/ru/ddc/gateway/controller/MeController.java
+++ gateway/src/main/java/ru/ddc/gateway/controller/MeController.java
@@ -15,19 +15,12 @@ public class MeController {
 
     @GetMapping("/me")
     public Map<String, Object> getUserInfo(@AuthenticationPrincipal OidcUser oidcUser) {
-        if (oidcUser == null) {
-            return Map.of("error", "Unauthorized");
-        }
-
-        Long expiresAtSeconds = oidcUser.getExpiresAt() != null
-                ? oidcUser.getExpiresAt().getEpochSecond()
-                : null;
 
         return Map.of(
                 "username", Objects.requireNonNull(oidcUser.getPreferredUsername()),
                 "email", Objects.requireNonNull(oidcUser.getEmail()),
                 "roles", oidcUser.getAuthorities(),
-                "exp", Objects.requireNonNull(expiresAtSeconds)
+                "exp", Objects.requireNonNull(oidcUser.getExpiresAt()).getEpochSecond()
         );
     }
 
