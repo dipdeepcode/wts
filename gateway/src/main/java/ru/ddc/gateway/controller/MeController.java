de7d3730d9bb6de0f067e0fc89faa2f196a43a5e
Sergey Kovalev <dipdeepcode@mail.ru>
2026-09-09 21:35:04 +0300

    debug(gateway): проверка получения ролей из токена с использованием конфигурации springaddons 3

--- gateway/src/main/java/ru/ddc/gateway/controller/MeController.java
+++ gateway/src/main/java/ru/ddc/gateway/controller/MeController.java
@@ -2,12 +2,14 @@ package ru.ddc.gateway.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 
@@ -17,12 +19,16 @@ public class MeController {
     private static final Logger logger = LoggerFactory.getLogger(MeController.class);
 
     @GetMapping("/me")
-    public Map<String, Object> getUserInfo(@AuthenticationPrincipal OidcUser oidcUser) {
+    public Map<String, Object> getUserInfo(Authentication authentication) {
+
+        if (authentication == null || !(authentication.getPrincipal() instanceof OidcUser oidcUser)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Not authenticated");
+        }
 
         var rawIdToken = oidcUser.getIdToken().getTokenValue();
         logger.info("Raw ID Token (JWT): {}", rawIdToken);
 
-        var roles = oidcUser.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+        var roles = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
         logger.info("Roles: {}", roles);
 
         return Map.of(
