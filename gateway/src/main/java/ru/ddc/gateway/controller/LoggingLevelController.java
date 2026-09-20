a7689e134c4acac2ea60ae73c62968485ead4bf8
Sergey Kovalev <dipdeepcode@mail.ru>
2026-09-20 19:07:10 +0300

    Revert "feat(gateway): добавлены TokenRefreshService и LoggingLevelController"
    This reverts commit 2be1e8851cdfd3b498d1d44fcdc3858a3f621726.

--- gateway/src/main/java/ru/ddc/gateway/controller/LoggingLevelController.java
+++ gateway/src/main/java/ru/ddc/gateway/controller/LoggingLevelController.java
@@ -13,7 +13,7 @@ import ru.ddc.gateway.service.TokenRefreshService;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/logging-level")
+@RequestMapping("/logging_level")
 public class LoggingLevelController {
     private final TokenRefreshService tokenRefreshService;
 
