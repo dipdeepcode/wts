9e70fc3625a0d69a98cbea9fecbbe1a34d05da38
Sergey Kovalev <dipdeepcode@mail.ru>
2026-09-20 19:07:08 +0300

    Revert "debug(gateway): отладка TokenRefreshService 2"
    This reverts commit 5ee03fc83c34413fe841d33e306c6e1f8250d51e.

--- gateway/src/main/java/ru/ddc/gateway/service/TokenRefreshService.java
+++ gateway/src/main/java/ru/ddc/gateway/service/TokenRefreshService.java
@@ -72,7 +72,6 @@ public class TokenRefreshService {
         log.info("refreshedClient: {}", refreshedClient);
 
         if (refreshedClient != null) {
-            log.info("Raw ID Token (JWT): {}", refreshedClient.getAccessToken().getTokenValue());
             // 4. Сохраняем обновленные токены обратно в Spring Session JDBC
             authorizedClientService.saveAuthorizedClient(refreshedClient, authentication);
         } else {
