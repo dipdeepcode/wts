01ba1ff4c8ea355a15eb20c8020af5f20a8f7dee
Sergey Kovalev <dipdeepcode@mail.ru>
2026-09-26 20:53:52 +0300

    Revert "feat(gateway): добавлено отключение csrf для /otel"
    This reverts commit 0a99d24512b4faaed985bb71a21c73a40200f2c2.

--- gateway/src/main/java/ru/ddc/gateway/configuration/WebSecurityConfig.java
+++ gateway/src/main/java/ru/ddc/gateway/configuration/WebSecurityConfig.java
@@ -1,9 +1,27 @@
 package ru.ddc.gateway.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain otelSecurityFilterChain(HttpSecurity http) {
+        http
+                .securityMatcher("/otel/**")
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
 }
